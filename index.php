f509a88e7952ae592b4f241f9b5355e271585016
SeanGS <steenhuis20@gmail.com>
2023-03-09 09:57:24 +0100

    basics

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main">
+        <div class="NavLogo">
+            <div class="logo">
+                <img src="pictures/birdlogo.png" alt="" height="100%" id="logo">
+            </div>
+            <div class="navigation"></div>
+        </div>
+
+        <div class="FeedSbar">
+            <div class="searchbar"></div>
+            <div class="feed"></div>
+        </div>
+
+        <div class="Friends">
+            
+        </div>
+
+    </div>
+</body>
+</html>
